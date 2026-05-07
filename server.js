efb13f0a6186c71d94302903da91266c09526ca9
Mirko Teschke <mirko.teschke@sap.com>
2026-05-07 10:35:28 -0700

    Attempt Fix

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const cds = require('@sap/cds');
+const path = require('path');
 
 cds.on('bootstrap', (app) => {
+  app.use('/com.mt.lotr.ui', require('express').static(path.join(__dirname, 'app/com.mt.lotr.ui/webapp')));
   app.get('/', (req, res) => res.redirect('/com.mt.lotr.ui/index.html'));
 });
 
